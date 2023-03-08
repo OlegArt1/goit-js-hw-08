fd3038824a04e5fa4df8cc2ec3ee1dbcf34683a1
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-03-08 12:54:10 +0300

    Updated file video.js

--- js/02-video.js
+++ js/02-video.js
@@ -4,9 +4,17 @@ const iframe = document.querySelector('iframe');
 
 const player = new Vimeo.Player(iframe);
 
+const widthContainer = (window.innerWidth - 640) / 2;
+
+const heightContainer = (window.innerHeight - 360 - 80) / 2;
+
 const LOCALSTORAGE_VIDEOPLAYER_KEY = "videoplayer-current-time";
 
 let time = 0;
+
+iframe.style.marginTop = heightContainer + 'px';
+
+iframe.style.marginLeft = widthContainer + 'px';
 
 player.on('play', (() =>
 {
@@ -16,34 +24,39 @@ player.on('timeupdate', function (time_update)
 {
     time = time_update.seconds;
 
-    console.log('\nTimeupdate - ', time + ' seconds; ' + time_update.percent + ' percent;');
+    let save_time = JSON.stringify({ 'seconds: ' : time + '; ', 'percent: ' : time_update.percent + ';' });
 
-    let saveTime = JSON.stringify(time);
+    localStorage.setItem(LOCALSTORAGE_VIDEOPLAYER_KEY, save_time);
 
-    localStorage.setItem(LOCALSTORAGE_VIDEOPLAYER_KEY, saveTime);
+    console.log("\nTimeupdate = ", time + " seconds; " + time_update.percent + " percent;");
+
+    console.log("\nDownload server - ", localStorage.getItem(LOCALSTORAGE_VIDEOPLAYER_KEY));
 });
 player.on('pause', (() =>
 {
     console.log('\nStop video!');
 
-    console.log('\nSet current time - ', time + ' seconds;');
+    console.log('\nSet current time = ', time + ' seconds;');
 }));
 player.setCurrentTime(time).then(() =>
+{
 
-{}).catch(function (error)
+}).catch(function (error)
 {
     switch (error.name)
     {
         case 'RangeError':
             
-            console.log('\nRangeE error - one of the camera properties is out of range\n');
+            console.log('\nRange error - one of the camera properties is out of range\n');
 
             break;
 
         default:
 
-            console.log('\nError - ' + error.message + '\n');
+            console.log('\nError name - ' + error.name + '\n');
             
+            console.log('\nError - ' + error.message + '\n');
+
             break;
     }
 });
